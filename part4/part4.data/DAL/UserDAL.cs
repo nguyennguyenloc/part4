61aa0afa4047d29a43e32ca92576463ee2c4d887
nguyennguyenloc <48286184+nguyennguyenloc@users.noreply.github.com>
2019-04-15 14:49:26 +0700

    Step2

---
+++ part4/part4.data/DAL/UserDAL.cs
@@ -0,0 +1,86 @@
+﻿using part4.data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part4.data.DAL
+{
+    public class UserDAL
+    {
+        private DefaultDbcontenxt context = new DefaultDbcontenxt();
+
+        public User GetByUsername(string username)
+        {
+            //Get from database
+            var user = context.Users
+                .Where(i => i.Username == username && i.IsDeleted == false)
+                .FirstOrDefault();
+            return user;
+        }
+
+        public bool Update(User model)
+        {
+            try
+            {
+                //Get item user with Id from database
+                var item = context.Users.Where(i => i.Id == model.Id).FirstOrDefault();
+
+                //Set value item with value from model
+                item.Username = model.Username;
+
+                //Save change to database
+                context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Create(User model)
+        {
+            try
+            {
+                //Initialization empty item
+                var item = new User();
+
+                //Set value for item with value from model
+                item.Username = model.Username;
+
+                //Add item to entity
+                context.Users.Add(item);
+                //Save to database
+                context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(long id)
+        {
+            try
+            {
+                //Tương tự update
+                var item = context.Users.SingleOrDefault(i => i.Id == id);
+
+                //Remove item.
+                context.Users.Remove(item);
+
+                //Change database
+                context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+
+}
---
+++ part4/part4.service/UserService.cs
@@ -0,0 +1,42 @@
+﻿using part4.core;
+using part4.data.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part4.service
+{
+    public class UserService
+    {
+        public bool LoginByCredential(string username, string password)
+        {
+            UserDAL userDAL = new UserDAL();
+
+            if (String.IsNullOrEmpty(username) || String.IsNullOrEmpty(password))
+            {
+                return false;
+            }
+
+            var user = userDAL.GetByUsername(username);
+            if (user == null)
+            {
+                return false;
+            }
+
+            var passwordSalt = user.PasswordSalt;
+            var passwordEncrypt = PasswordHash.EncryptionPasswordWithSalt(password, passwordSalt);
+            if (passwordEncrypt == user.PasswordEncrypted)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+
+}
